135108bd2ca27e2bc5a9e32e402d3f893fcbde4a
jackson-callaghan-school <jackson-callaghan-school@users.noreply.github.com>
2019-12-07 14:14:27 -0500

    fix read_data (again)

--- group2package/R/hello.R
+++ group2package/R/hello.R
@@ -1,15 +1,24 @@
+require(tidyverse)
 
-# read data
+
+#' Import data from file.
+#' Create sex binary column for later use.
+#'
+#' @param filename a csv file
+#'
+#' @return a tibble of read data
+#'
+#' @examples
+#' read_data("titanic_group_project.csv")
 read_data <- function(filename) {
-  # read the data and transform to prep for logistic regression
-  titanic_data <- read.csv(filename)
-  titanic_data$sex <- as.character(titanic_data$sex)
-  titanic_data <- titanic_data %>% mutate(sex_binary = ifelse(sex == "female", 1, 0))
+  temp_data <- read_csv(filename)
+  temp_data$sex <- as.character(temp_data$sex)
+  temp_data <- temp_data %>% mutate(sex_binary = ifelse(sex == "female", 1, 0))
 
-  return(titanic_data)
+  return(temp_data)
 }
 
-#Task 1
+# Task 1
 
 logistic_regression_model_1 <- function(data_set) {
 
@@ -18,27 +27,29 @@ logistic_regression_model_1 <- function(data_set) {
     formula = survived ~ pclass + sex_binary + age,
     data = data_set,
     family = binomial
-  ) #need to write function to be conducive to all data sets or just this one?
+  ) # need to write function to be conducive to all data sets or just this one?
 }
 
-#need to have the logistic_regression_model function as an argument here
+# need to have the logistic_regression_model function as an argument here
 prob_survival <- function(pclass, sex_binary, age) {
 
-  #take the outputs of the logistic_regression_model_1 function and calculate the probability of survival
+  # take the outputs of the logistic_regression_model_1 function and calculate the probability of survival
   log_reg_output <- logistic_regression_model_1(pclass, sex_binary, age)
 
-  log_odds_survival <- (summary(log_reg_output)$coefficients[1,1])+
-    ((summary(log_reg_output)$coefficients[2,1])*pclass)+((summary(log_reg_output)$coefficients[3,1])*sex_binary)+
-    ((summary(log_reg_output)$coefficients[4,1])*age)
+  log_odds_survival <- (summary(log_reg_output)$coefficients[1,1]) +
+    (summary(log_reg_output)$coefficients[2,1] * pclass) +
+    (summary(log_reg_output)$coefficients[3,1] * sex_binary)+
+    (summary(log_reg_output)$coefficients[4,1] * age)
 
-  #log_odds_survival <- 4.58927-(1.13324*pclass)-(2.49738*sexmale)-(0.03388*age)
+  # log_odds_survival <- 4.58927-(1.13324*pclass)-(2.49738*sexmale)-(0.03388*age)
 
-    probs_survival <- exp(log_odds_survival)/(1+(exp(log_odds_survival)))
+    probs_survival <- exp(log_odds_survival) / (1 + (exp(log_odds_survival)))
 
     return(probs_survival)
+
     }
 
-#Task 2
+# Task 2
 
 survival_stats <- function(data_set) {
 
